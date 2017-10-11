fae4cf193b18c4f6fd049ee9032933f8e56f91a5
aaronandriod <aaronmuzira@gmail.com>
2017-10-11 12:58:38 +0300

    Initial commit

---
+++ app/src/main/java/com/aaroncoders/datepicker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.aaroncoders.datepicker;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+    }
+
+    public void showDatePicker(View view) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int date = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                tv.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+            }
+        }, year, month, date);
+        dpd.show();
+    }
+}
+
+
